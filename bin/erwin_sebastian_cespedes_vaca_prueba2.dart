96553164d57427c2d5b649050c84124602235da9
ErwinSebastian <escespedes2024@incos-santacruz.edu.bo>
2026-04-20 20:25:03 -0400

    Initial commit

--- bin/erwin_sebastian_cespedes_vaca_prueba2.dart
+++ bin/erwin_sebastian_cespedes_vaca_prueba2.dart
@@ -1,5 +1,4 @@
 import 'package:erwin_sebastian_cespedes_vaca_prueba2/erwin_sebastian_cespedes_vaca_prueba2.dart' as erwin_sebastian_cespedes_vaca_prueba2;
-
 void main(List<String> arguments) {
   print('Hello world: ${erwin_sebastian_cespedes_vaca_prueba2.calculate()}!');
 }
